f349a59b691b0112766f722b9374fda830ba77bb
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-12-11 08:46:26 +0100

    Programa Terminado

---
+++ Ejercicio13_2/src/ejercicio13_2/ConversorTemperaturas.java
@@ -0,0 +1,59 @@
+package ejercicio13_2;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author Arturo
+ */
+public class ConversorTemperaturas{
+
+    private static float t=80f;
+    private String tempetatura;
+    public float centigrados, fahrenheit=0, reamur=0;
+
+    public ConversorTemperaturas(){
+    }
+
+//    public float pedirTemperatura(){
+//        tempetatura=JOptionPane.showInputDialog(null, "Introduzca la temperatura en ºC");
+//        centigrados=Float.parseFloat(tempetatura);
+//        return centigrados;
+//    }
+    public float centigradosAFahrenheit() throws TemperatureException{
+        tempetatura=JOptionPane.showInputDialog(null, "Introduzca la temperatura en ºC");
+        centigrados=Float.parseFloat(tempetatura);
+        if(centigrados>t){
+            fahrenheit=(float) (9.0/5.0*centigrados+32.4);
+            return fahrenheit;
+        }else{
+            throw new TemperatureException("La Temperatura debe ser superior a 80ºC");
+        }
+    }
+
+    public float centigradosAReamur(){
+        do{
+            tempetatura=JOptionPane.showInputDialog(null, "Introduzca la temperatura en ºC");
+            centigrados=Float.parseFloat(tempetatura);
+        }while(centigrados<=t);
+        try{
+            if(centigrados>t){
+                reamur=(float) (4.0/5.0*centigrados);
+            }else{
+                throw new TemperatureException("La Temperatura debe ser superior a 80ºC");
+            }
+        }catch(TemperatureException e){
+            e.printStackTrace();
+        }finally{
+            return reamur;
+        }
+    }
+
+    @Override
+    public String toString(){
+        if(centigrados>t){
+            return (centigrados+" ºC Son:\n"+fahrenheit+" ºF y "+reamur+" ºR");
+        }else{
+            return ("Error");
+        }
+    }
+}

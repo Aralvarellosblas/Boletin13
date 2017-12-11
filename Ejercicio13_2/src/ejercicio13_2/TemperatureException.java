16952594f04fbe7d199fd87073e0f03ab9fc654c
Aralvarellosblas <arturo.alvarellos@gmail.com>
2017-12-11 08:46:37 +0100

    Programa Terminado

---
+++ Ejercicio13_2/src/ejercicio13_2/TemperatureException.java
@@ -0,0 +1,11 @@
+package ejercicio13_2;
+
+/**
+ * @author Arturo
+ */
+public class TemperatureException extends Exception{
+
+    public TemperatureException(String mensaje){
+        super(mensaje);
+    }
+}
